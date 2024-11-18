af9a888ceff8223dda663b98f364596e991a6ca2
Tin test <dima20_2003@mail.ru>
2024-11-18 16:48:21 +0300

    Completed Django assignment

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -1,17 +1,31 @@
 import csv
+import os
 
 from django.core.management.base import BaseCommand
 from phones.models import Phone
 
 
 class Command(BaseCommand):
+    help = 'Import phones data from a CSV file'
     def add_arguments(self, parser):
-        pass
+        parser.add_argument('csv_file', type='str')
 
     def handle(self, *args, **options):
+        csv_file_path = options['csv_file']
+        if not os.path.exists('csv_file_path'):
+            print(f'Error: File {csv_file_path} not found')
+            return
+
         with open('phones.csv', 'r') as file:
             phones = list(csv.DictReader(file, delimiter=';'))
 
-        for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+        for phone_data in phones:
+            phone = Phone(
+                name=phone_data['name'],
+                price=phone_data['price'],
+                image=phone_data['image'],
+                release_date=phone_data['release_date'],
+                lte_exists=phone_data['lte_exists'].lower() == 'true',
+            )
+            phone.save()
+            print(f"Phone '{phone_data['name']}' imported successfully.")
--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,20 @@
 from django.db import models
-
+from django.urls import reverse
+from django.utils.text import slugify
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=40)
+    price = models.FloatField()
+    image = models.ImageField(upload_to='phones_images')
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+
+    slug = models.SlugField(max_length=200, unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify.slufify(self.name)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url_(self):
+        return reverse('phone_detail', kwargs={'slug': self.slug})
